450bf2ce6119390466adce8346846a4be6ced4d2
Pascal Wichmann <git@pa-w.de>
2017-09-21 16:08:22 +0200

    Definitions: Add Drupal

---
+++ definitions/drupal.py
@@ -0,0 +1,19 @@
+import re
+
+from definitions.definition import SoftwareDefinition
+from providers.git import GitTagProvider
+from software_package import SoftwarePackage
+
+
+class Drupal(SoftwareDefinition):
+    software_package = SoftwarePackage(
+        name='Drupal',
+        vendor='Drupal')
+    provider = GitTagProvider(
+        software_package=software_package,
+        url='https://github.com/drupal/drupal.git',
+        exclude_pattern=re.compile(r'start')
+    )
+    path_map = {
+        '/': '/',
+    }
