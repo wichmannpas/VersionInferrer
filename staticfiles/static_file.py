136c21469f14cfc67fcfa5e44f8962dd6c86c073
Pascal Wichmann <git@pa-w.de>
2017-10-03 11:41:20 +0200

    Add first draft for StaticFile abstraction

---
+++ staticfiles/static_file.py
@@ -0,0 +1,41 @@
+from abc import abstractproperty, ABCMeta
+from typing import List
+
+
+class StaticFile(metaclass=ABCMeta):
+    """
+    This is the abstract base class for a static file of a specific type.
+
+    The interface is used to pre- and post-process static files and to
+    determine whether a file is a static file (of a specific type).
+    """
+    USUAL_FILE_NAME_EXTENSIONS: List[str]
+
+    file_name: str
+    raw_content: bytes
+
+    def __init__(self, file_name: str, raw_content: bytes):
+        self.file_name = file_name
+        self.raw_content = raw_content
+
+        if not self.matches_file_type:
+            raise ValueError(
+                'not a static file of type {}'.format(self.__class__.__name__)
+            )
+
+    @property
+    def has_usual_file_name_extension(self) -> bool:
+        """Whether the file has a usual file name extension."""
+        return any(
+            self.file_name.endswith('.' + extension)
+            for extension in self.USUAL_FILE_NAME_EXTENSIONS)
+
+    @abstractproperty
+    def matches_file_type(self) -> bool:
+        """Whether the current instance is a static file of this type."""
+
+    @abstractproperty
+    def normalized_content(self) -> bytes:
+        """
+        The content of this static file normalized for this file type.
+        """
