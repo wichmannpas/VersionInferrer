463935f84ddbc52852ca4315739e623788f86755
Pascal Wichmann <git@pa-w.de>
2017-11-26 15:24:51 +0100

    Add yaml file definition

---
+++ files/yaml_file.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+from files.file import File
+
+
+class YamlFile(File):
+    """
+    A YAML file.
+    """
+    USUAL_FILE_NAME_EXTENSIONS = [
+        'yaml',
+        'yml',
+    ]
+    USE_FOR_ANALYSIS = True
+    USE_FOR_INDEX = True
+
+    @property
+    def matches_file_type(self) -> bool:
+        """Whether the current instance is a static file of this type."""
+        return self.has_usual_file_name_extension
+
+    @property
+    def normalized_content(self) -> Union[bytes, None]:
+        """
+        The content of this static file normalized for this file type.
+        """
+        # TODO: do actual normalization
+        return self.raw_content
