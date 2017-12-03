df7b3728f08fa53d54dabd921d9ea6d9d2cb107a
Pascal Wichmann <git@pa-w.de>
2017-12-03 11:21:51 +0100

    Add python data normalization

--- base/utils.py
+++ base/utils.py
@@ -1,6 +1,7 @@
 import os
 from urllib.parse import urljoin
 
+import msgpack
 from url_normalize import url_normalize
 
 
@@ -27,3 +28,30 @@ def join_url(*args) -> str:
         path = join_paths(*args[1:])
 
     return url_normalize(urljoin(base_url, path))
+
+
+def normalize_data(data: object) -> bytes:
+    """
+    Normalize Python data into a bytes string.
+
+    The purpose of the normalization is to transform data into
+    its canonical form, thus making similar data which
+    is supposed to be identical actually identical.
+    """
+    if isinstance(data, bytes):
+        return data
+    if isinstance(data, str):
+        return data.encode()
+    if isinstance(data, dict):
+        return msgpack.dumps(sorted([
+            (normalize_data(key), normalize_data(value))
+            for key, value in data.items()
+        ]))
+    if isinstance(data, (list, set, tuple)):
+        return msgpack.dumps(sorted([
+            normalize_data(item)
+            for item in data
+        ]))
+
+    # use msgpack without further normalization. Might raise TypeError
+    return msgpack.dumps(data)
