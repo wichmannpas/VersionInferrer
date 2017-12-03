5961a122ce500937bba03ba97311ef3f8539c61b
Pascal Wichmann <git@pa-w.de>
2017-12-03 12:15:33 +0100

    Use normalization for yaml and json file

--- files/json_file.py
+++ files/json_file.py
@@ -1,5 +1,7 @@
+import json
 from typing import Union
 
+from base.utils import normalize_data
 from files.file import File
 
 
@@ -16,6 +18,9 @@ class JsonFile(File):
     @property
     def matches_file_type(self) -> bool:
         """Whether the current instance is a static file of this type."""
+        if self.raw_content is not None and self.normalized_content is None:
+            # normalization fails
+            return False
         return self.has_usual_file_name_extension
 
     @property
@@ -23,5 +28,8 @@ class JsonFile(File):
         """
         The content of this static file normalized for this file type.
         """
-        # TODO: do actual normalization
-        return self.raw_content
+        try:
+            data = json.loads(self.raw_content.decode())
+            return normalize_data(data)
+        except (TypeError, ValueError):
+            return None
--- files/yaml_file.py
+++ files/yaml_file.py
@@ -1,5 +1,9 @@
 from typing import Union
 
+import yaml
+from yaml.error import YAMLError
+
+from base.utils import normalize_data
 from files.file import File
 
 
@@ -17,6 +21,9 @@ class YamlFile(File):
     @property
     def matches_file_type(self) -> bool:
         """Whether the current instance is a static file of this type."""
+        if self.raw_content is not None and self.normalized_content is None:
+            # normalization fails
+            return False
         return self.has_usual_file_name_extension
 
     @property
@@ -24,5 +31,8 @@ class YamlFile(File):
         """
         The content of this static file normalized for this file type.
         """
-        # TODO: do actual normalization
-        return self.raw_content
+        try:
+            data = yaml.load(self.raw_content.decode())
+            return normalize_data(data)
+        except (TypeError, ValueError, YAMLError):
+            return None
