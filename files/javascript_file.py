fdd5a694d8483d1623d87a32acf3a56a0586931c
Pascal Wichmann <git@pa-w.de>
2017-12-28 10:18:01 +0100

    Javascript: Try to construct abstract syntax tree

--- files/javascript_file.py
+++ files/javascript_file.py
@@ -1,5 +1,10 @@
+import logging
 from typing import Union
 
+from slimit import ast
+from slimit.parser import Parser
+
+from base.utils import normalize_data
 from files.file import File
 
 
@@ -16,6 +21,9 @@ class JavascriptFile(File):
     @property
     def matches_file_type(self) -> bool:
         """Whether the current instance is a static file of this type."""
+        if self.raw_content is not None and self.normalized_content is None:
+            # normalization fails
+            return False
         return self.has_usual_file_name_extension
 
     @property
@@ -23,5 +31,50 @@ class JavascriptFile(File):
         """
         The content of this static file normalized for this file type.
         """
-        # TODO: do actual normalization
-        return self.raw_content
+        try:
+            content = self.raw_content.decode()
+        except UnicodeDecodeError:
+            return None
+
+        if not self.has_usual_file_name_extension:
+            # skip parsing if file extension does not indicate javascript
+            return None
+
+        parser = Parser()
+
+        try:
+            parsed = parser.parse(content)
+            parsed = ast_to_builtin(parsed)
+
+            return normalize_data(parsed)
+        except (SyntaxError, TypeError, ValueError):
+            logging.warning(
+                'failed to parse javascript file %s. Skipping abstract syntax tree construction',
+                self.file_name)
+            parsed = content
+        try:
+            return normalize_data(parsed)
+        except (TypeError, ValueError):
+            return None
+
+
+def ast_to_builtin(data: object) -> object:
+    """
+    Model slimit.ast objects using only  built-in types.
+
+    This makes use of the __dict__ attribute of the python objects.
+    """
+    if isinstance(data, ast.Node):
+        return ast_to_builtin(data.__dict__)
+
+    if isinstance(data, dict):
+        return {
+            key: ast_to_builtin(value)
+            for key, value in data.items()
+        }
+    elif isinstance(data, (list, set, tuple)):
+        return [
+            ast_to_builtin(item)
+            for item in data
+        ]
+    return data
